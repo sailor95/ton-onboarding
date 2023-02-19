6a3c2e34de93841105cb08288e8d6bfbb30b0b42
David Huang <bugman195@gmail.com>
2023-02-19 16:48:51 +0800

    Init API provider

--- index.ts
+++ index.ts
@@ -1,2 +1,15 @@
+import { Address, TonClient } from 'ton'
+import * as dotenv from 'dotenv';
 
-// write your NFT miner here
+dotenv.config(); // To use env variable
+
+async function main() {
+  const wallet = Address.parse(process.env.MY_TON_ADDRESS as string)
+  const collection = Address.parse('EQDk8N7xM5D669LC2YACrseBJtDyFqwtSPCNhRWXU7kjEptX')
+  const client = new TonClient({
+    endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+    apiKey: process.env.API_KEY
+  })
+}
+
+main()
