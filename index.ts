8baf45c3886e7293f61f392c2ae910e4f39d0d23
David Huang <bugman195@gmail.com>
2023-02-19 16:56:14 +0800

    Show mining data

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Address, TonClient } from 'ton'
 import * as dotenv from 'dotenv';
+import {BN} from 'bn.js'
 
 dotenv.config(); // To use env variable
 
@@ -10,6 +11,22 @@ async function main() {
     endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
     apiKey: process.env.API_KEY
   })
+  const miningData = await client.callGetMethod(collection, 'get_mining_data')
+
+  const parseStackNum = (sn: any) => new BN(sn[1].substring(2), 'hex');
+  const complexity = parseStackNum(miningData.stack[0]);
+  const last_success = parseStackNum(miningData.stack[1]);
+  const seed = parseStackNum(miningData.stack[2]);
+  const target_delta = parseStackNum(miningData.stack[3]);
+  const min_cpl = parseStackNum(miningData.stack[4]);
+  const max_cpl = parseStackNum(miningData.stack[5]);
+
+  console.log('complexity', complexity);
+  console.log('last_success', last_success.toString());
+  console.log('seed', seed);
+  console.log('target_delta', target_delta.toString());
+  console.log('min_cpl', min_cpl.toString());
+  console.log('max_cpl', max_cpl.toString());
 }
 
 main()
